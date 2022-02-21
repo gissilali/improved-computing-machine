5398a02a58f23cb574f6ed8309f2e1d125ebbecc
Gibson Silali <gibsonsilali@gmail.com>
2022-02-21 09:40:49 +0300

    ...

---
+++ .env.js
@@ -0,0 +1,5 @@
+ const env = {
+    API_BACKEND_URL: "https://enigmatic-caverns-06740.herokuapp.com/api",
+}
+
+export default env
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,51 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
+import env from "../.env.js";
+import { useState, useEffect } from "react";
+
+const baseUrl = env.API_BACKEND_URL;
 
 export default function Home() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch(baseUrl + "/books")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+
   return (
     <div className={styles.container}>
-      <h1>BookTok</h1>
+     <div className="card card-bordered card-preview">
+        <div className="card-header">
+          <h3 className="card-title">Books</h3>
+        </div>
+        <table className="table table-tranx">
+          <thead>
+          <tr className="tb-tnx-item">
+              <th>ISBN #</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Release Date</th>
+              <th>Character Count</th>
+            </tr>
+          </thead>
+          <tbody>
+          {books.map((book) => (
+            <tr key={book.isbn} className="tb-tnx-item">
+              <td>{book.isbn}</td>
+              <td>{book.authors[0]}</td>
+              <td>{book.release_date}</td>
+              <td>{book.name}</td>
+              <td>
+                {book.character_count} <a href={`/characters/${book.isbn}`}>(view all)</a>
+              </td>
+
+            </tr>
+          ))}
+            
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
